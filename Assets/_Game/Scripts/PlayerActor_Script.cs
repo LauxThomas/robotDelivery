858d5c7086fb3f01dbd8496ae1b73db9c28b4cd4
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2019-03-25 14:27:45 +0100

    Created Actor and InputProvider Script

---
+++ Assets/_Game/Scripts/PlayerActor_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActor_Script : MonoBehaviour
+{
+	[SerializeField] private GameObject _gameObjectTop;
+	[SerializeField] private GameObject _gameObjectBottom;
+	[SerializeField] private GameObject _gameObjectPlayer;
+
+	public Rigidbody _rigidbodyTop { get; private set; }
+	public Rigidbody _rigidbodyBottom { get; private set; }
+
+	private void Awake()
+	{
+		_rigidbodyTop = _gameObjectTop.GetComponent<Rigidbody>();
+		_rigidbodyBottom = _gameObjectBottom.GetComponent<Rigidbody>();
+	}
+
+	// Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+    }
+}
