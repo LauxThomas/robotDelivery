a7923b9e13409f8ef2a2228269860fceabf97017
BATCOM\chgae <chgaerths@googlemail.com>
2019-03-26 17:21:25 +0100

    Added TMP Assets;

---
+++ Assets/_Game/Scripts/UI/PackageLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+using UnityEditor;
+
+
+public class PackageLoader : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Image packageImage;
+    [SerializeField]
+    private Button next;
+    [SerializeField]
+    private Button previous;
+    [SerializeField]
+    private TextMeshProUGUI description;
+    [SerializeField]
+    private Package[] package;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void onClickNext(){
+        description.SetText("Hallo");
+    }
+}
