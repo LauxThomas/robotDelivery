3361c90cc4390d7f4e4667db0b31133c31b183d2
Thomas Laux <laux.thomas@t-online.de>
2019-03-26 10:52:14 +0100

    Scoregrundgerüst eingefügt

--- Assets/_Game/Scripts/UI/ScoreController.cs
+++ Assets/_Game/Scripts/UI/ScoreController.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public float score;
+	public float slowingFactor=5;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	//public Transform scoreText;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		updateScoreText();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		score += Time.deltaTime*1/slowingFactor;
+		updateScoreText();
+	}
+
+	private void updateScoreText()
+	{
+		gameObject.GetComponent<TextMeshProUGUI>().text = "Score: " + score.ToString("F2");
+		//scoreText.GetComponent<TextMeshProUGUI>().text = "Score: " + (int) score;
+	}
+
+	public void addScore(int val)
+	{
+		score += val;
+		updateScoreText();
+	}
 }
