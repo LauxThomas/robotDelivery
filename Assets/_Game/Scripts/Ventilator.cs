a41fdf882b6fd5d13e103e0cebe67dab99673cdb
Johannes H <johannesherfurth@web.de>
2019-03-26 11:21:56 +0100

    Ventilator Add Force

--- Assets/_Game/Scripts/Ventilator.cs
+++ Assets/_Game/Scripts/Ventilator.cs
@@ -1,32 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Boo.Lang;
 using UnityEngine;
 
 public class Ventilator : MonoBehaviour
 {
 
 	[SerializeField] private float force;			//Force of the  Ventilator
-
+	private Vector3 ventForce;
+	private Rigidbody rbd;
 
 	private void Awake()
 	{
+		Vector3 ventForce = new Vector3(force, 0, 0);
+
 	}
 
-	private void OnCollisionEnter(Collision other)
-    {
-	    other.transform.LookAt(-transform.position);
-	    Debug.Log("Enter");
-    }
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "player")
+		{
+			Rigidbody rbd = other.GetComponent<Rigidbody>();
+			rbd.AddForce(ventForce);
+			Debug.Log("Enter");
+		}
+	}
 
-    private void OnCollisionStay(Collision other)
+
+
+    private void OnTriggerStay(Collider other)
     {
-	    other.transform.Translate(-transform.position);
+
+
 	    Debug.Log("Stay");
     }
 
-    private void OnCollisionExit(Collision other)
+    private void OnTriggerExit(Collider other)
     {
-	    other.transform.Translate(0,0,0);
-	    Debug.Log("Exit");
+	    if (other.tag == "player")
+	    {
+		    rbd.AddForce(ventForce);
+		    Debug.Log("Exit");
+	    }
+
     }
 }
