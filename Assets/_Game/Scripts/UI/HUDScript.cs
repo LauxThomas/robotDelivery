87484b123fcaf3fda8d54912e320dbda392e1689
Thomas Laux <laux.thomas@t-online.de>
2019-03-27 14:33:49 +0100

    V2

--- Assets/_Game/Scripts/UI/HUDScript.cs
+++ Assets/_Game/Scripts/UI/HUDScript.cs
@@ -5,16 +5,10 @@ using UnityEngine;
 public class HUDScript : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
-	    //TODO:TOGGLEFUNKTIONEN!
 	    if (Time.timeScale>0)
 	    {
 		    toggleHUD(true);
