489e49e90b04693d5b01674f2d6d4056f31596b3
Johannes H <johannesherfurth@web.de>
2019-03-27 14:34:13 +0100

    WIP Score PickUp

--- Assets/scrip.cs
+++ Assets/scrip.cs
@@ -2,19 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HUDScript : MonoBehaviour
+public class scrip : MonoBehaviour
 {
-
-	//if(){}
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
