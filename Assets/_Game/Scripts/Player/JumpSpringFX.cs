8b2c2981db78078d01fd9a610bbdf1dd714bc3c4
Sebastian Krause <sebastian@hatita.com>
2019-03-28 11:10:15 +0100

    spring fx component

--- Assets/_Game/Scripts/Player/JumpSpringFX.cs
+++ Assets/_Game/Scripts/Player/JumpSpringFX.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Changes color of the springs emission based on supplied tension.
+ */
 public class JumpSpringFX : MonoBehaviour
 {
+	public Color BaseColor;
+	public Color TensionColor;
+
+	public AnimationCurve TensionCurve;
+
+	private Material emissionMaterial;
+
 	// Start is called before the first frame update
 	void Start()
 	{
