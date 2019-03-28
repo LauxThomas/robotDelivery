df19ce3dfa3130a8ca8e08315d696b25a2f948c7
Sebastian Krause <sebastian@hatita.com>
2019-03-28 11:47:18 +0100

    animation curves as assets & hdr blending as asset

--- Assets/_Game/Scripts/Player/JumpSpringFX.cs
+++ Assets/_Game/Scripts/Player/JumpSpringFX.cs
@@ -7,22 +7,49 @@ using UnityEngine;
  */
 public class JumpSpringFX : MonoBehaviour
 {
-	public Color BaseColor;
-	public Color TensionColor;
+	public HDRColorBlendAsset ColorBlendAsset;
+	public string PropertyName = "_EmissionColor";
 
-	public AnimationCurve TensionCurve;
-
+	private int PropertyId;
 	private Material emissionMaterial;
 
 	// Start is called before the first frame update
-	void Start()
+	void Awake()
 	{
-
+		emissionMaterial = GetComponent<Renderer>().material;
+		PropertyId = Shader.PropertyToID(PropertyName);
 	}
 
-	// Update is called once per frame
-	void Update()
+	 
+	/// <summary>
+	/// Determinates color of supplied tension argument.
+	/// </summary>
+	/// <param name="InTension">Tension parameter in range of [0,1]</param>
+	/// <returns>The resulting color from tension</returns>
+	public Color ApplyTension(float InTension)
 	{
+		InTension = Mathf.Clamp01(InTension);
+		Color resultingColor = ColorBlendAsset.Lerp(InTension);
+		emissionMaterial.SetColor(PropertyId, resultingColor);
+
+		return resultingColor;
+	}
+
+	float X = 0.0F;
+	float Y = -1.0F;
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Y = 1F;
+		}
+		if (Input.GetKeyUp(KeyCode.Space))
+		{
+			Y = -1F;
+		}
+
+		X = Mathf.Clamp01(X + Time.deltaTime * Y);
+		ApplyTension(X);
 
 	}
 }
