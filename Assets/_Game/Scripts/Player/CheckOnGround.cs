78587a34980336a5a00b86890d392bb85a3e9231
Tobias Schmidt <tobiasschmidt.torgelow@gmail.com>
2019-03-29 19:48:36 +0100

    Added Raycast for Groundcheck

--- Assets/_Game/Scripts/Player/CheckOnGround.cs
+++ Assets/_Game/Scripts/Player/CheckOnGround.cs
@@ -13,13 +13,33 @@ public class CheckOnGround : MonoBehaviour
 		_playerActorScript = playerActorPrefab.GetComponent<PlayerActor_Script>();
 	}
 
+	/*
 	private void OnCollisionEnter(Collision other)
 	{
+		Debug.Log(other);
 		_playerActorScript.isGrounded = true;
 	}
 
 	private void OnCollisionExit(Collision other)
 	{
 		_playerActorScript.isGrounded = false;
+	}
+	*/
+
+	private void Update()
+	{
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.4f))
+		{
+			_playerActorScript.isGrounded = true;
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
+			Debug.Log("Did Hit");
+		}
+		else
+		{
+			_playerActorScript.isGrounded = false;
+			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 1000, Color.white);
+			Debug.Log("Did not Hit");
+		}
 	}
 }
